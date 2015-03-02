d91c099430851fea084c4b9fcd6b610f812a95b8
Jestersage <gleung4@gmail.com>
2015-03-02 10:09:32 -0800

    more code

--- GoodSamaritan/GoodSamaritan/Models/Users/Client.cs
+++ GoodSamaritan/GoodSamaritan/Models/Users/Client.cs
@@ -12,6 +12,21 @@ namespace GoodSamaritan.Models.Users
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ClientId { get; set; }
 
+        //FiscalYear Lookup
+
+        //int Month
+        //int Day
+        //string Surname
+        //string FirstName
+        //string PoliceFileNum
+        //int CourtFileNum
+        //int SWCFileNum
+
+        //RiskLevel LOOKUP
+        //Crisis LOOKUP
+        //Service LOOKUP
+        //Program LOOKUP
+        //RiskAccessmentAssignedTo LOOKUP
 
     }
 }
