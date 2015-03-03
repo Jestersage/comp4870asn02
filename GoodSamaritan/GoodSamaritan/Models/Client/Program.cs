8caca3cc14d49280d74a847166828de5855310ff
Jestersage <gleung4@gmail.com>
2015-03-03 13:50:55 -0800

    more entry

---
+++ GoodSamaritan/GoodSamaritan/Models/Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoodSamaritan.Models.Client
+{
+    public class Program
+    {
+        public int ProgramId { get; set; }
+        
+        //Crisis; Court; SMART; DVU; MCFD
+        //If Program == SMART, ref Smart
+        public string ProgramName { get; set; }
+    }
+}
