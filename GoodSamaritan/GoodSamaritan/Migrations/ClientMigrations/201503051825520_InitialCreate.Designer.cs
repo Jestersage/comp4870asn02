4f8f2515ef757f903d7198810a03b77406b2952c
Jestersage <gleung4@gmail.com>
2015-03-05 11:59:45 -0800

    more code

--- GoodSamaritan/GoodSamaritan/Migrations/ClientMigrations/201503051825520_InitialCreate.Designer.cs
+++ GoodSamaritan/GoodSamaritan/Migrations/ClientMigrations/201503051825520_InitialCreate.Designer.cs
@@ -7,13 +7,13 @@ namespace GoodSamaritan.Migrations.ClientMigrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.1-30610")]
-    public sealed partial class TestCreate : IMigrationMetadata
+    public sealed partial class InitialCreate : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(TestCreate));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
         
         string IMigrationMetadata.Id
         {
-            get { return "201503050418427_TestCreate"; }
+            get { return "201503051825520_InitialCreate"; }
         }
         
         string IMigrationMetadata.Source
