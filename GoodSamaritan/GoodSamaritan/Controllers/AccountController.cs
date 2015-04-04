3a98984b14c938346a1152b2711129eb03bef7a1
Jestersage <gleung4@gmail.com>
2015-04-03 23:58:18 -0700

    update to a3_v1

--- GoodSamaritan/GoodSamaritan/Controllers/AccountController.cs
+++ GoodSamaritan/GoodSamaritan/Controllers/AccountController.cs
@@ -75,7 +75,28 @@ namespace GoodSamaritan.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: true);
+
+            var user = await UserManager.FindAsync(model.Email, model.Password);
+
+            if (user != null)
+            {
+                if (UserManager.IsInRole(user.Id, "Administrator"))
+                {
+                    Session["Admin"] = "Administrator";
+                }
+
+                if (UserManager.IsInRole(user.Id, "Worker"))
+                {
+                    Session["Worker"] = "Worker";
+                }
+
+                if (UserManager.IsInRole(user.Id, "Reporter"))
+                {
+                    Session["Reporter"] = "Reporter";
+                }
+            }
+
             switch (result)
             {
                 case SignInStatus.Success:
