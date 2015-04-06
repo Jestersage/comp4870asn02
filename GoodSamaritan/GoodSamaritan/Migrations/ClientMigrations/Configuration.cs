8a8056f52ff3991e190a5fabbed6aaec0595ac79
Jestersage <gleung4@gmail.com>
2015-04-06 00:03:51 -0700

    Redo controllers.

--- GoodSamaritan/GoodSamaritan/Migrations/ClientMigrations/Configuration.cs
+++ GoodSamaritan/GoodSamaritan/Migrations/ClientMigrations/Configuration.cs
@@ -47,20 +47,15 @@ namespace GoodSamaritan.Migrations.ClientMigrations
                 Clients.ToArray()
             );
              * */
-
+            //10-11; 11-12; 12-13; 13-14; 14-15; 15-16; 16-17
             List<FiscalYear> FiscalYears = new List<FiscalYear>() {
-                new FiscalYear { 
-                  //10-11; 11-12; 12-13; 13-14; 14-15; 15-16; 16-17
-                FiscalYearName="10-11",
-                },
-                new FiscalYear { 
-                  //10-11; 11-12; 12-13; 13-14; 14-15; 15-16; 16-17
-                FiscalYearName="11-12",
-                },
-                new FiscalYear { 
-                  //10-11; 11-12; 12-13; 13-14; 14-15; 15-16; 16-17
-                FiscalYearName="12-13",
-                },
+                new FiscalYear { FiscalYearName="10-11" },
+                new FiscalYear { FiscalYearName="11-12" },
+                new FiscalYear { FiscalYearName="12-13" },
+                new FiscalYear { FiscalYearName="13-14" },
+                new FiscalYear { FiscalYearName="14-15" },
+                new FiscalYear { FiscalYearName="15-16" },
+                new FiscalYear { FiscalYearName="16-17" },
               };
 
             context.FiscalYears.AddOrUpdate(
