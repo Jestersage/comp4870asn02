9008c760ffcc634b9e8185f362cad46d341350d4
Jestersage <gleung4@gmail.com>
2015-04-08 16:09:09 -0700

    Update db

--- AngularJS/app.js
+++ AngularJS/app.js
@@ -1,20 +1,5 @@
 ﻿var app = angular.module('myApp', []);
 
-app.controller('MainController', function ($scope, $http) {
-    var onUserComplete = function (response) {
-        $scope.user = response.data;
-    };
-
-    var onError = function (reason) {
-        $scope.error = "Could not fetch the GitHub user.";
-        //$scope.error = reason;
-    };
-
-    $http.get("https://api.github.com/users/shanselman")
-      .then(onUserComplete, onError);
-
-    $scope.message = "Hello, Angular!";
-});
 
 
 app.controller('ClientController', function ($scope, $http) {
