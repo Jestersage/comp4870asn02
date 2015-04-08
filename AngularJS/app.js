f4c7399e31722517144ea49a93367b3e5218d0b6
Jestersage <gleung4@gmail.com>
2015-04-08 13:30:58 -0700

    Update db

--- AngularJS/app.js
+++ AngularJS/app.js
@@ -31,11 +31,15 @@ app.controller('MainController', function ($scope, $http) {
 app.controller('ClientController', function ($scope, $http) {
 
     var onClientComplete = function (response) {
-        $scope.user = response;
+        console.log(JSON.stringify(response));
+        $scope.user = response.data;
+        $scope.dump = JSON.stringify(response.data);
+
     };
 
     var onError = function (reason) {
         $scope.error = "Could not fetch the Client.";
+        $scope.dump = JSON.stringify(response.data);
         //$scope.error = reason;
     };
 
